b1d634bb10e64d5f7f20d814bf1427404706b4b8
Lee Thomas <lee@ahluka.co.uk>
2021-04-23 12:31:35 +0100

    initial commit

---
+++ src/scenes/Game.js
@@ -0,0 +1,94 @@
+import Phaser from '../lib/phaser.js'
+
+export default class Game extends Phaser.Scene
+{
+    /** @type {Phaser.Physics.Arcade.Sprite} */
+    player
+
+    /** @type {Phaser.Physics.Arcade.StaticGroup} */
+    platforms
+
+    /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
+    cursors
+
+
+    constructor() {
+        super('game')
+    }
+
+    preload() {
+        this.load.image('background', 'assets/bg_layer1.png')
+        this.load.image('platform', 'assets/ground_grass.png')
+        this.load.image('bunny-stand', 'assets/bunny1_stand.png')
+
+        this.cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    create() {
+        this.add.image(240, 320, 'background').setScrollFactor(1, 0)
+
+        this.platforms = this.physics.add.staticGroup()
+
+        for (let i = 0; i < 5; i++) {
+            const x = Phaser.Math.Between(80, 400)
+            const y = 150 * i
+            const platform = this.platforms.create(x, y, 'platform')
+            platform.scale = 0.5
+
+            const body = platform.body
+            body.updateFromGameObject()
+        }
+
+        this.player = this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5)
+        this.physics.add.collider(this.platforms, this.player)
+        this.player.body.checkCollision.up = false
+        this.player.body.checkCollision.left = false
+        this.player.body.checkCollision.right = false
+
+        this.cameras.main.startFollow(this.player)
+        this.cameras.main.setDeadzone(this.scale.width * 1.5)
+    }
+
+    update(t, dt) {
+        this.platforms.children.iterate(child => {
+            /** @type {Phaser.Physics.Arcade.Sprite} */
+            const plat = child
+            const scrollY = this.cameras.main.scrollY
+
+            if (plat.y >= scrollY + 700) {
+                plat.y = scrollY - Phaser.Math.Between(50, 100)
+                plat.body.updateFromGameObject()
+            }
+        })
+
+        const touchingDown = this.player.body.touching.down
+
+        if (touchingDown) {
+            this.player.setVelocityY(-300)
+        }
+
+        if (this.cursors.left.isDown && !touchingDown) {
+            this.player.setVelocityX(-200)
+        }
+        else if (this.cursors.right.isDown && !touchingDown) {
+            this.player.setVelocityX(200)
+        }
+        else {
+            this.player.setVelocityX(0)
+        }
+
+        this.horizontalWrap(this.player)
+    }
+
+    horizontalWrap(sprite) {
+        const halfW = sprite.displayWidth * 0.5
+        const gameW = this.scale.width
+
+        if (sprite.x < -halfW) {
+            sprite.x = gameW + halfW
+        }
+        else if (sprite.x > gameW + halfW) {
+            sprite.x = -halfW
+        }
+    }
+}
